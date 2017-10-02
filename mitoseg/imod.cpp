0fbf7e52451a0bf695eb089ad00a4aaf389971f4
Serdar Tasel <fst@cankaya.edu.tr>
2017-10-02 23:23:11 +0300

    IMOD support

--- mitoseg/imod.cpp
+++ mitoseg/imod.cpp
@@ -93,66 +93,67 @@ void savePolyArrayAsIMOD(poly25d *p, int n, int x_size, int y_size, int z_size,
 			}
 		}
 		// Get mesh info
-		/*		int (**faces)[3] = (int (**)[3]) calloc(p[k].t - 1, sizeof(int (*)[3]));
-		 int *n_faces = (int *) calloc(p[k].t - 1, sizeof(int));
-		 int n_vert = p[k].slice[0].n;
-		 int n_totalf = 0;
-		 for (i = 0; i < p[k].t - 1; i++) {
-		 n_faces[i] = getTriangles(&p[k].slice[i], &p[k].slice[i + 1],
-		 &faces[i]);
-		 n_vert += p[k].slice[i + 1].n;
-		 n_totalf += n_faces[i];
-		 }
-		 // Write vertices
-		 id = IMOD_VALUE(IMOD_MESH_ID);
-		 fwrite(&id, sizeof(int), 1, f);
-		 memset(&mesh, 0, sizeof(imod_mesh));
-		 mesh.vsize = IMOD_VALUE(n_vert);
-		 mesh.lsize = IMOD_VALUE(n_totalf * 5 + 1);
-		 fwrite(&mesh, sizeof(imod_mesh), 1, f);
-		 for (i = 0; i < p[k].t; i++) {
-		 for (j = 0; j < p[k].slice[i].n; j++) {
-		 pt = (float) (p[k].slice[i].vertex[j].x * xy_scale + x_shift);
-		 pt = IMOD_VALUE(pt);
-		 fwrite(&pt, sizeof(float), 1, f);
-		 pt = (float) (y_size - 1 - p[k].slice[i].vertex[j].y * xy_scale
-		 - y_shift);
-		 pt = IMOD_VALUE(pt);
-		 fwrite(&pt, sizeof(float), 1, f);
-		 pt = (float) ((p[k].start_z + i) * z_scale);
-		 pt = IMOD_VALUE(pt);
-		 fwrite(&pt, sizeof(float), 1, f);
-		 }
-		 }
-		 // Write triangles
-		 int count = 0;
-		 for (i = 0; i < p[k].t - 1; i++) {
-		 for (j = 0; j < p[k].slice[i].n; j++) {
-		 id = IMOD_VALUE(-25);
-		 fwrite(&id, sizeof(int), 1, f);
-		 id = IMOD_VALUE(faces[i][j][0] + count);
-		 fwrite(&id, sizeof(int), 1, f);
-		 id = IMOD_VALUE(faces[i][j][1] + count);
-		 fwrite(&id, sizeof(int), 1, f);
-		 id = IMOD_VALUE(faces[i][j][2] + count);
-		 fwrite(&id, sizeof(int), 1, f);
-		 id = IMOD_VALUE(-22);
-		 fwrite(&id, sizeof(int), 1, f);
-		 }
-		 count += p[k].slice[i].n;
-		 }
-		 id = IMOD_VALUE(-1);
-		 fwrite(&id, sizeof(int), 1, f);
-		 // Free mem.
-		 if (n_faces) {
-		 free(n_faces);
-		 }
-		 for (int i = 0; i < p[k].t - 1; i++) {
-		 if (faces[i])
-		 free(faces[i]);
-		 }
-		 if (faces) {
-		 free(faces);}*/
+		int (**faces)[3] = (int (**)[3]) calloc(p[k].t - 1, sizeof(int (*)[3]));
+		int *n_faces = (int *) calloc(p[k].t - 1, sizeof(int));
+		int n_vert = p[k].slice[0].n;
+		int n_totalf = 0;
+		for (i = 0; i < p[k].t - 1; i++) {
+			n_faces[i] = getTriangles(&p[k].slice[i], &p[k].slice[i + 1],
+					&faces[i]);
+			n_vert += p[k].slice[i + 1].n;
+			n_totalf += n_faces[i];
+		}
+		// Write vertices
+		id = IMOD_VALUE(IMOD_MESH_ID);
+		fwrite(&id, sizeof(int), 1, f);
+		memset(&mesh, 0, sizeof(imod_mesh));
+		mesh.vsize = IMOD_VALUE(n_vert);
+		mesh.lsize = IMOD_VALUE(n_totalf * 5 + 1);
+		fwrite(&mesh, sizeof(imod_mesh), 1, f);
+		for (i = 0; i < p[k].t; i++) {
+			for (j = 0; j < p[k].slice[i].n; j++) {
+				pt = (float) (p[k].slice[i].vertex[j].x * xy_scale + x_shift);
+				pt = IMOD_VALUE(pt);
+				fwrite(&pt, sizeof(float), 1, f);
+				pt = (float) (y_size - 1 - p[k].slice[i].vertex[j].y * xy_scale
+						- y_shift);
+				pt = IMOD_VALUE(pt);
+				fwrite(&pt, sizeof(float), 1, f);
+				pt = (float) ((p[k].start_z + i) * z_scale);
+				pt = IMOD_VALUE(pt);
+				fwrite(&pt, sizeof(float), 1, f);
+			}
+		}
+		// Write triangles
+		int count = 0;
+		for (i = 0; i < p[k].t - 1; i++) {
+			for (j = 0; j < n_faces[i]; j++) {
+				id = IMOD_VALUE(-25);
+				fwrite(&id, sizeof(int), 1, f);
+				id = IMOD_VALUE(faces[i][j][0] + count);
+				fwrite(&id, sizeof(int), 1, f);
+				id = IMOD_VALUE(faces[i][j][1] + count);
+				fwrite(&id, sizeof(int), 1, f);
+				id = IMOD_VALUE(faces[i][j][2] + count);
+				fwrite(&id, sizeof(int), 1, f);
+				id = IMOD_VALUE(-22);
+				fwrite(&id, sizeof(int), 1, f);
+			}
+			count += p[k].slice[i].n;
+		}
+		id = IMOD_VALUE(-1);
+		fwrite(&id, sizeof(int), 1, f);
+		// Free mem.
+		if (n_faces) {
+			free(n_faces);
+		}
+		for (int i = 0; i < p[k].t - 1; i++) {
+			if (faces[i])
+				free(faces[i]);
+		}
+		if (faces) {
+			free(faces);
+		}
 	}
 	// EOF
 	id = IMOD_VALUE(IMOD_IEOF_ID);
